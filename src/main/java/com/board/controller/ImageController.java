df5c4bf7bc1aa71f1aadac97821a56630bf35d7d
Correct Name <correct-skm99086@gmail.com>
2024-05-14 19:47:39 +0900

    skin

--- src/main/java/com/board/controller/ImageController.java
+++ src/main/java/com/board/controller/ImageController.java
@@ -15,89 +15,28 @@ import java.io.InputStreamReader;
 import java.nio.file.*;
 
 @RestController
-@RequestMapping("/api/images")
 public class ImageController {
 
     @Autowired
     private ImageRepository imageRepository;
 
-    @PostMapping
-    public ApiResponseDto<Long> uploadImage(@RequestParam("image") MultipartFile multipartFile) throws IOException {
+    @PostMapping("/api/images/pet")
+    public ResponseEntity<String> PetImage(@RequestParam("image") MultipartFile multipartFile) throws IOException, InterruptedException {
         String fileName = multipartFile.getOriginalFilename();
-        Path path = Paths.get("C:/Users/skm99/OneDrive/Desktop/Img/" + fileName);
+        Path path = Paths.get("/home/ubuntu/animalcareplus/image/" + fileName);
         Files.write(path, multipartFile.getBytes());
 
         Image image = new Image();
         image.setFileName(fileName);
         imageRepository.save(image);
 
-        return ApiResponseDto.<Long>builder()
-                .success(true)
-                .response(image.getId())
-                .build();
-    }
-
-        @GetMapping("/{id}")
-        public ResponseEntity<String> predictBreed(@PathVariable Long id) throws Exception {
-            Image image = imageRepository.findById(id)
-                    .orElseThrow(() -> new IllegalArgumentException("Invalid image ID: " + id));
-
-            // 이미지 파일 경로
-            Path path = Paths.get("/home/ubuntu/animalcareplus/image/" + image.getFileName());
-            String imagePath = path.toString();
-
-            // 파이썬 스크립트 경로
-            String pythonScriptPath = "/home/ubuntu/animalcareplus/python_ai/pet.py";
-
-            String result = "";
-            // 파이썬 실행 명령
-            String[] cmd = {"python", pythonScriptPath, imagePath};
-
-            ProcessBuilder pb = new ProcessBuilder(cmd);
-            Process process = pb.start();
-
-            BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            BufferedReader err = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-
-            String line;
-            StringBuilder output = new StringBuilder();
-            StringBuilder errorMessage = new StringBuilder();
-
-            String lastLine = null;
-            while ((line = in.readLine()) != null) {
-                output.append(line).append("\n");
-                lastLine = line; // 각 줄을 읽을 때마다 마지막 줄을 갱신합니다.
-            }
-
-            result = lastLine; // 마지막으로 읽은 줄을 결과로 설정합니다.
-
-            while ((line = err.readLine()) != null) {
-                errorMessage.append(line).append("\n");
-            }
-
-            int exitCode = process.waitFor();
-
-//        System.out.println("Exit code: " + exitCode);
-//            result = output.toString();
-//        System.out.println("Error: " + errorMessage.toString());
-
-            return ResponseEntity.ok()
-                    .contentType(MediaType.TEXT_PLAIN)
-                    .body(result);
-        }
-
-
-    @GetMapping("/img/{id}")
-    public ResponseEntity<String> predictBreed2(@PathVariable Long id) throws Exception {
-        Image image = imageRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid image ID: " + id));
-
         // 이미지 파일 경로
-        Path path = Paths.get("/home/ubuntu/animalcareplus/image/" + image.getFileName());
-        String imagePath = path.toString();
+        Path paths = Paths.get("/home/ubuntu/animalcareplus/image/" + image.getFileName());
+        String imagePath = paths.toString();
 
         // 파이썬 스크립트 경로
-        String pythonScriptPath = "/home/ubuntu/animalcareplus/python_ai/SkinDisease.py";
+        String pythonScriptPath = "/home/ubuntu/animalcareplus/python_ai/pet.py";
+
 
         String result = "";
         // 파이썬 실행 명령
@@ -127,26 +66,69 @@ public class ImageController {
 
         int exitCode = process.waitFor();
 
-        System.out.println("Exit code: " + exitCode);
+//        System.out.println("Exit code: " + exitCode);
 //            result = output.toString();
-        System.out.println("Error: " + errorMessage.toString());
+//        System.out.println("Error: " + errorMessage.toString());
 
         return ResponseEntity.ok()
                 .contentType(MediaType.TEXT_PLAIN)
                 .body(result);
     }
-//    @GetMapping("/{id}")
-//    public ResponseEntity<byte[]> getImage(@PathVariable Long id) throws IOException {
-//        Image image = imageRepository.findById(id)
-//                .orElseThrow(() -> new IllegalArgumentException("Invalid image ID: " + id));
-//        //파일 경로
-//        Path path = Paths.get("C:/Users/skm99/OneDrive/Desktop/Img" + image.getFileName());
-//        byte[] imageBytes = Files.readAllBytes(path);
+    @PostMapping("/api/images/skin")
+    public ResponseEntity<String> SkinImage(@RequestParam("image") MultipartFile multipartFile) throws IOException, InterruptedException {
+        String fileName = multipartFile.getOriginalFilename();
+        Path path = Paths.get("/home/ubuntu/animalcareplus/image/" + fileName);
+        Files.write(path, multipartFile.getBytes());
+
+        Image image = new Image();
+        image.setFileName(fileName);
+        imageRepository.save(image);
+
+        // 이미지 파일 경로
+        Path paths = Paths.get("/home/ubuntu/animalcareplus/image/" + image.getFileName());
+        String imagePath = paths.toString();
+
+        // 파이썬 스크립트 경로
+        String pythonScriptPath = "/home/ubuntu/animalcareplus/python_ai/pet.py";
+
+
+        String result = "";
+        // 파이썬 실행 명령
+        String[] cmd = {"python", pythonScriptPath, imagePath};
+
+        ProcessBuilder pb = new ProcessBuilder(cmd);
+        Process process = pb.start();
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        BufferedReader err = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+
+        String line;
+        StringBuilder output = new StringBuilder();
+        StringBuilder errorMessage = new StringBuilder();
+
+        String lastLine = null;
+        while ((line = in.readLine()) != null) {
+            output.append(line).append("\n");
+            lastLine = line; // 각 줄을 읽을 때마다 마지막 줄을 갱신합니다.
+        }
+
+        result = lastLine; // 마지막으로 읽은 줄을 결과로 설정합니다.
+
+        while ((line = err.readLine()) != null) {
+            errorMessage.append(line).append("\n");
+        }
+
+        int exitCode = process.waitFor();
+
+//        System.out.println("Exit code: " + exitCode);
+//            result = output.toString();
+//        System.out.println("Error: " + errorMessage.toString());
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.TEXT_PLAIN)
+                .body(result);
+    }
 //
-//        return ResponseEntity.ok()
-//                .contentType(MediaType.IMAGE_JPEG)
-//                .body(imageBytes);
-//    }
 
     private String getFileExtension(String fileName) {
         int dotIndex = fileName.lastIndexOf(".");
